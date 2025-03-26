8df3072ed33ff2d8054ea1ff912c8fbd1eb63edb
Sean Khottavong <sean.khottavong@gmail.com>
2025-03-26 11:15:06 -0400

    Updating urls

--- maintenance.php
+++ maintenance.php
@@ -23,9 +23,9 @@ header('Retry-After: 600');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="icon" href="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/uploads/media/favicon.png">
-    <link rel="stylesheet" href="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/maintenance/assets/styles.css">
-    <script src="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/maintenance/assets/timer.js"></script>
+    <link rel="icon" href="https://lwff.theoric.dev/wp-content/uploads/media/favicon.png">
+    <link rel="stylesheet" href="https://lwff.theoric.dev/wp-content/maintenance/assets/styles.css">
+    <script src="https://lwff.theoric.dev/wp-content/maintenance/assets/timer.js"></script>
     <title>Scheduled Maintenance</title>
 </head>
 
@@ -44,13 +44,13 @@ header('Retry-After: 600');
     <!--START_SOCIAL_LINKS_BLOCK-->
     <section class="social-links">
                     <a class="social-links__link" href="https://www.facebook.com/cPanel" target="_blank" title="Facebook">
-                <span class="icon"><img src="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/maintenance/assets/images/facebook.svg" alt="Facebook"></span>
+                <span class="icon"><img src="https://lwff.theoric.dev/wp-content/maintenance/assets/images/facebook.svg" alt="Facebook"></span>
             </a>
                     <a class="social-links__link" href="https://twitter.com/cPanel" target="_blank" title="Twitter">
-                <span class="icon"><img src="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/maintenance/assets/images/twitter.svg" alt="Twitter"></span>
+                <span class="icon"><img src="https://lwff.theoric.dev/wp-content/maintenance/assets/images/twitter.svg" alt="Twitter"></span>
             </a>
                     <a class="social-links__link" href="https://instagram.com/cPanel" target="_blank" title="Instagram">
-                <span class="icon"><img src="https://wordpress-1244601-5367116.cloudwaysapps.com/wp-content/maintenance/assets/images/instagram.svg" alt="Instagram"></span>
+                <span class="icon"><img src="https://lwff.theoric.dev/wp-content/maintenance/assets/images/instagram.svg" alt="Instagram"></span>
             </a>
             </section>
     <!--END_SOCIAL_LINKS_BLOCK-->
